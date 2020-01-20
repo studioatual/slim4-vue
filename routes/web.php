05e504e4c2a67794b307a6433251486d2f4c2745
Studio Atual <contato@studioatual.com>
2020-01-20 20:28:44 -0300

    Add DotEnv + Config File + PHP-DI

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,10 @@
 use Psr\Http\Message\RequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 
-$app->get('[/]', function (Request $request, Response $response) {
-  $response->getBody()
-    ->write('ok');
-  return $response;
+$app->get('[/]', function (Request $request, Response $response) use ($app) {
+    $appName = $app->getContainer()->get('settings')['app']['name'];
+
+    $response->getBody()
+        ->write($appName);
+    return $response;
 })->setName('home');
