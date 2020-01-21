fd943c8f95c8783020c5eba6deea56cb9484230f
Studio Atual <contato@studioatual.com>
2020-01-21 01:48:20 -0300

    Configure Database and Create User Model

--- app/Controllers/Web/HomeController.php
+++ app/Controllers/Web/HomeController.php
@@ -3,11 +3,14 @@
 namespace StudioAtual\Controllers\Web;
 
 use StudioAtual\Controllers\Controller;
+use StudioAtual\Models\User;
 
 class HomeController extends Controller
 {
     public function index()
     {
+        //$users = User::all();
+        //return $this->withJson($users);
         //return $this->withJson(['result' => 'ok']);
         return $this->view->render($this->response, 'index.html', []);
     }
