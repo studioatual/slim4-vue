e387be90ab592b20b266bc5316666e944c6d1ab8
Studio Atual <contato@studioatual.com>
2020-01-27 07:02:55 -0300

    Replace ContainerBuilder to DI\Container with Csrf Config

--- bootstrap/mail.php
+++ bootstrap/mail.php
@@ -1,22 +1,19 @@
 <?php
 
-use Psr\Container\ContainerInterface;
 use StudioAtual\Mail\Mailer;
 
-$containerBuilder->addDefinitions([
-    'mailer' => function (ContainerInterface $c) {
-        if ($c->get('settings')['mail']['driver'] == 'smtp') {
-            $transport = (new Swift_SmtpTransport(
-                $c->get('settings')['mail']['host'],
-                $c->get('settings')['mail']['port']
-            ))
-                ->setEncryption($c->get('settings')['mail']['encryption'])
-                ->setAuthMode($c->get('settings')['mail']['auth'])
-                ->setUsername($c->get('settings')['mail']['username'])
-                ->setPassword($c->get('settings')['mail']['password']);
-        } else {
-            $transport = new Swift_SendmailTransport("/usr/sbin/sendmail -bs");
-        }
-        return new Mailer($c->get('view'), new Swift_Mailer($transport));
+$container->set('mailer', function ($c) {
+    if ($c->get('settings')['mail']['driver'] == 'smtp') {
+        $transport = (new Swift_SmtpTransport(
+            $c->get('settings')['mail']['host'],
+            $c->get('settings')['mail']['port']
+        ))
+            ->setEncryption($c->get('settings')['mail']['encryption'])
+            ->setAuthMode($c->get('settings')['mail']['auth'])
+            ->setUsername($c->get('settings')['mail']['username'])
+            ->setPassword($c->get('settings')['mail']['password']);
+    } else {
+        $transport = new Swift_SendmailTransport("/usr/sbin/sendmail -bs");
     }
-]);
+    return new Mailer($c->get('view'), new Swift_Mailer($transport));
+});
